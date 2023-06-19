d1f02693144e63f3c484fadc0b5af5f50115af1f
aslon1213 <hamidovaslon1@gmail.com>
2023-06-20 01:31:48 +0500

    some bug fixes on dockercompose

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"fmt"
+	"net/http"
 	"os"
+	"time"
 
 	"github.com/aslon1213/reservations-API/handlers"
 	"github.com/aslon1213/reservations-API/models"
@@ -38,7 +42,50 @@ func Init() {
 
 }
 
+func create_some_rooms() {
+
+	http_client := &http.Client{}
+	type raw_input struct {
+		Name     string
+		Type     string
+		Capacity int
+	}
+
+	var raw_inputs = []raw_input{
+		{"mytaxi", "focus", 1},
+		{"workly", "team", 5},
+		{"express24", "conference", 15},
+		{"Focus Room 1", "focus", 1},
+		{"Focus Room 2", "focus", 1},
+		{"Focus Room 3", "focus", 1},
+	}
+
+	for _, raw_input := range raw_inputs {
+		// wait for 10 seconds
+		time.Sleep(5 * time.Second)
+
+		// make request to create room
+
+		raw_date := []byte(fmt.Sprintf(`{"name":"%s","type":"%s","capacity":%d}`, raw_input.Name, raw_input.Type, raw_input.Capacity))
+
+		req, err := http.NewRequest("POST", "http://localhost:8080/api/rooms/new", bytes.NewBuffer(raw_date))
+		if err != nil {
+			panic(err)
+		}
+		req.Header.Set("Content-Type", "application/json")
+		resp, err := http_client.Do(req)
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+
+	}
+}
+
 func main() {
+
+	time.Sleep(time.Second * 30)
+
 	Init()
 	router := gin.Default()
 
@@ -52,6 +99,7 @@ func main() {
 	api.DELETE("/reservs/delete/all", Handlers.DeleteAllReservations)
 	api.GET("/rooms/:id/reservations_full", Handlers.GetAllReservationsFullInfo)
 	api.DELETE("/rooms/:id/unbook", Handlers.UnbookRoom)
+	go create_some_rooms()
 	if err := router.Run("0.0.0.0:8080"); err != nil {
 		panic(err)
 	}
